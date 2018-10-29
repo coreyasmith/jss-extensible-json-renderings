eebfbd2b42d4d430d19e4459a94f48db51d1d683
Corey Smith <coreyas@gmail.com>
2018-10-29 01:15:41 -0400

    Add flag to JSON Renderings to add ATLSUG members

---
+++ src/Feature/AtlSug/code/Pipelines/LayoutService/TransformPlaceholderElement/AddAtlSugMembers.cs
@@ -0,0 +1,34 @@
+﻿using CoreySmith.Foundation.LayoutService.ItemRendering;
+using CoreySmith.Foundation.LayoutService.Pipelines.LayoutService.TransformPlaceholderElement;
+using Sitecore.Data.Fields;
+
+namespace CoreySmith.Feature.AtlSug.Pipelines.LayoutService.TransformPlaceholderElement
+{
+  public class AddAtlSugMembers : TransformPlaceholderElementProcessor
+  {
+    public override void TransformPlaceholderElement(TransformPlaceholderElementPipelineArgs args)
+    {
+      if (!(args.Element is ExtensibleRenderedJsonRendering extensibleRendering)) return;
+      if (!ShouldAddAtlSugMembers(extensibleRendering)) return;
+
+      args.Result.atlSugMembers = new[]
+      {
+        "George \"Sitecore George\" Chang",
+        "Martin \"Sitecore Artist\" English",
+        "Anastasiya \"JavaScript Ninja\" Flynn",
+        "Varun \"Too Many Questions\" Nehra",
+        "Craig \"From ATL\" Taylor",
+        "Amy \"Sitecore Amy\" Winburn"
+      };
+    }
+
+    private static bool ShouldAddAtlSugMembers(ExtensibleRenderedJsonRendering extensibleRendering)
+    {
+      var rendering = extensibleRendering.Rendering?.RenderingItem?.InnerItem;
+      if (rendering == null) return false;
+
+      var addAtlSugMembersField = (CheckboxField)rendering.Fields[Templates.JsonRendering.Fields.AddAtlSugMembers];
+      return addAtlSugMembersField?.Checked ?? false;
+    }
+  }
+}
---
+++ src/Feature/AtlSug/code/Templates.cs
@@ -0,0 +1,13 @@
+﻿namespace CoreySmith.Feature.AtlSug
+{
+  public struct Templates
+  {
+    public struct JsonRendering
+    {
+      public struct Fields
+      {
+        public static string AddAtlSugMembers = "{05C24A58-91B7-458D-A664-AB5F463DBB57}";
+      }
+    }
+  }
+}
